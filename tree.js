e53e979ef6785f79e5db8f7f1000ff807f39c05c
Hitsuki9 <ybfqdjb7788@gmail.com>
2018-10-09 23:38:36 +0800

    树

---
+++ tree.js
@@ -0,0 +1,19 @@
+//二叉搜索树 es6 实现私有属性，但无法继承
+const BinarySearchTree = (function () {
+    let params = new WeakMap();
+    class Node {
+        constructor (key) {
+            this.key = key;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    class BinarySearchTree {
+        constructor () {
+            params.set(this, {
+                root: null
+            });
+        }
+    }
+    return BinarySearchTree;
+})();
