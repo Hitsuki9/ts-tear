52436525002593e7ebeba8356024aaf4cf55c235
wangxin <wangxin@thothinfo.com>
2019-12-31 18:01:36 +0800

    update: EventEmitter

--- api/EventEmitter/EventEmitter.ts
+++ api/EventEmitter/EventEmitter.ts
@@ -1,1 +1,28 @@
-export default class EventEmitter {}
+export default class EventEmitter {
+  events: Map<string, Function[]> = new Map();
+  maxListeners = 10;
+  addListener(type: string, fn: Function) {
+    const handlers = this.events.get(type);
+    if (handlers) {
+      if (handlers.length >= this.maxListeners) {
+        console.warn(
+          `can't add listener because the maxListeners is ${this.maxListeners}`
+        );
+        return;
+      }
+      handlers.push(fn);
+      this.events.set(type, handlers);
+    } else {
+      this.events.set(type, [fn]);
+    }
+  }
+  emit(type: string, ...args: any[]) {
+    const handlers = this.events.get(type);
+    if (handlers) {
+      for (let i = 0; i < handlers.length; i++) {
+        handlers[i](...args)
+      }
+    }
+    return true;
+  }
+}
