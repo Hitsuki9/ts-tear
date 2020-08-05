46d705b9281bf3f16470d39d71c69e08a0d33ce4
wangxin <wangxin@thothinfo.com>
2020-08-05 13:59:24 +0800

    union find version 2

--- algorithm/union-find/UF.ts
+++ algorithm/union-find/UF.ts
@@ -1,32 +1,40 @@
 /**
- * 并查集
- * TODO: union and find
+ * 加权并查集
  */
 export default class UF {
   private id: number[];
+  private sz: number[]; // 各个根节点所对应的分量的大小（权重）
   count: number;
 
   constructor(N: number) {
     this.count = N;
     this.id = new Array(N);
+    this.sz = new Array(N);
     for (let i = 0; i < N; i++) {
       this.id[i] = i;
+      this.sz[i] = 1;
     }
   }
 
   union(p: number, q: number) {
-    const pID = this.find(p);
-    const qID = this.find(q);
-    if (qID === pID) return this;
-    this.id.forEach((id, idx) => {
-      if (id === pID) this.id[idx] = qID;
-    });
+    const i = this.find(p);
+    const j = this.find(q);
+    if (i === j) return this;
+    // 将小树的根节点连接到大树的根节点
+    if (this.sz[i] < this.sz[j]) {
+      this.id[i] = j;
+      this.sz[j] += this.sz[i];
+    } else {
+      this.id[j] = i;
+      this.sz[i] += this.sz[j];
+    }
     this.count--;
     return this;
   }
 
   find(p: number) {
-    return this.id[p];
+    while (p !== this.id[p]) p = this.id[p];
+    return p;
   }
 
   connected(p: number, q: number) {
